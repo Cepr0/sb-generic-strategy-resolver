c61b237ab0f21517f659a06591acdc6f889f6897
Sergei Poznanski <cepr0@ukr.net>
2019-08-03 20:30:01 +0300

    Simplify DataController

--- src/main/java/io/github/cepr0/demo/DataController.java
+++ src/main/java/io/github/cepr0/demo/DataController.java
@@ -3,14 +3,12 @@ package io.github.cepr0.demo;
 import io.github.cepr0.demo.data.Data;
 import io.github.cepr0.demo.data.DataTO;
 import io.github.cepr0.demo.strategy.DataHandlerContext;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Slf4j
 @RestController
 @RequestMapping("/data")
 public class DataController {
@@ -22,7 +20,7 @@ public class DataController {
 	}
 
 	@PostMapping
-	public <D extends Data> ResponseEntity post(@RequestBody DataTO<D> dto) {
+	public ResponseEntity post(@RequestBody DataTO<Data> dto) {
 		return dataHandlerContext.handle(dto.getData())
 				.map(ResponseEntity::ok)
 				.orElse(ResponseEntity.notFound().build());
